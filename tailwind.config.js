79367ca8d0a5b9cdd9d27f0ace89e28b1a753d3f
sam1132 <117072712+sam1132@users.noreply.github.com>
2023-09-08 12:57:02 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+      'cream': '#edd2a4'
+      },
+    }
+  },
+  plugins: [],
+}
+
